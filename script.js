3d732b24d449bcd4e06031620b6b60d44434c479
casagrande2023 <143603533+casagrande2023@users.noreply.github.com>
2025-04-11 01:48:59 +0000

    aula 6

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -16,3 +17,11 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+
+ const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
