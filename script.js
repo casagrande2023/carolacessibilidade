9b793585098481a5db931f742a169fbf31b7ae37
casagrande2023 <143603533+casagrande2023@users.noreply.github.com>
2025-04-25 01:47:49 +0000

    aula 6

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -16,12 +19,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 
-});
 
- const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-
-botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+   });
+});
