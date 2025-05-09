ae2958d11c7ba5661a88a71e2c0c19493a92051d
casagrande2023 <143603533+casagrande2023@users.noreply.github.com>
2025-05-09 01:46:41 +0000

    aula7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    constDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     botaoDeAcessibilidade.addEventListener('click', function () {
@@ -7,6 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
     })
+    
+//Código omitido
+
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+
+//Código omitido
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
